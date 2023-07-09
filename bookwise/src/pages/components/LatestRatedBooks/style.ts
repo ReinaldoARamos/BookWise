f841c331b840bc072bc2364ea1b0210ceedddcd5
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-07-09 13:19:07 -0300

    ajeitando melhor o desing do site

--- bookwise/src/pages/components/LatestRatedBooks/style.ts
+++ bookwise/src/pages/components/LatestRatedBooks/style.ts
@@ -2,16 +2,18 @@ import { styled } from "../../../../stiches.config";
 
 export const LatestBooksContainer = styled("div", {
   width: 608,
-  marginRight: "1rem",
+  marginLeft: '-12.5rem',
 backgroundColor: 'Orange',
   header: {
     marginBottom: "2.5rem",
   },
 
   section: {
+
     fontFamily: "$default",
     color: "$gray100",
     fontWeight: "$regular",
-    fontSize: '$md'
+    fontSize: '$md',
+  
   },
 });;
