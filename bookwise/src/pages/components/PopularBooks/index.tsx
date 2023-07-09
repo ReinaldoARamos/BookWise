82352ac6390dbdec295104faf7f59b17312a9d0c
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-07-09 14:44:49 -0300

    criando o componente de livros mais bem avaliados

--- bookwise/src/pages/components/PopularBooks/index.tsx
+++ bookwise/src/pages/components/PopularBooks/index.tsx
@@ -1,13 +1,35 @@
-import { PopularContainer } from "./style";
-import { CaretRight} from 'phosphor-react'
-export function PopularBooks() {
-    return (
-     <PopularContainer>
-        <header>
-        <section>Livros mais populares</section>
-        <div>Ver todos <CaretRight /></div>            
-        </header>
+import { CaretRight, Star } from "phosphor-react";
+import { PopularContainer, PopularBookContent, PopularBookContainer } from "./style";
+import Image from "next/image";
+import BookExample from "../../../../public/Books/o-hobbit.png";
 
-     </PopularContainer>
+export function PopularBook() {
+  
+    return (
+        <PopularContainer>
+      <header>
+        <section>Livros mais populares</section>
+        <div>
+          Ver todos <CaretRight />
+        </div>
+      </header>
+      <PopularBookContainer>
+        <Image src={BookExample} alt="" width={64} height={94}/>
+        <PopularBookContent>
+          <section>
+            O Hobbit
+            <div>J. R. R. Tolkien</div>
+          </section>
+
+          <p>
+            <Star size={14} weight="fill" />
+            <Star size={14} weight="fill" />
+            <Star size={14} weight="fill" />
+            <Star size={14} weight="fill" />
+            <Star size={14} />
+          </p>
+        </PopularBookContent>
+      </PopularBookContainer>
+    </PopularContainer>
     )
 }
--- bookwise/src/pages/components/PopularBooks/style.ts
+++ bookwise/src/pages/components/PopularBooks/style.ts
@@ -3,20 +3,67 @@ import { styled } from "../../../../stiches.config";
 
 export const PopularContainer = styled("div", {
     paddingTop: "6.85rem",
-
     
-    width: 324,
+    width: '20.25rem',
    
   header: {
-    width: '20rem',
-   
-    
+    width: '20.25rem',
     display: 'flex',
     justifyContent: 'space-between',
     alignItems: 'center',
     fontFamily: "$default",
     color: "$gray100",
     fontWeight: "$regular",
-    fontSize: '$md'
+    fontSize: '$md',
+    marginBottom: "2rem",
+    div: {
+      fontWeight: "$bold",
+      color: '$purple100'
+    }
   },
+  
+
+
 });
+
+
+export const PopularBookContainer = styled("div", {
+  width: '20.25rem',
+  height: '8.125rem',
+  backgroundColor: '$gray700',
+  borderRadius: 8,
+  padding: '1rem',
+  display: 'flex',
+
+  section: {
+   
+    fontFamily: '$default',
+    color: '$gray100',
+    fontWeight: "$bold",
+    
+    fontSize: '$md',
+  div: {
+    marginTop: "0.2rem",
+    fontFamily: '$default',
+    color: '$gray400',
+    fontSize: '$xs',
+}
+   
+  },
+
+})
+
+export const PopularBookContent = styled('div', {
+  display:'flex',
+  flexDirection: 'column',
+
+  justifyContent: 'space-between',
+  width: '100%',
+  marginLeft: '1rem',
+
+  p: {
+    svg: {
+      color: '$purple100'
+    }
+  }
+})
