ae3113ea3802e6b60733366579e4ed1f9598793e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-07-15 20:13:50 -0300

    criando a base do perfilzinho

--- bookwise/src/pages/user/profile/components/UserInfo/index.tsx
+++ bookwise/src/pages/user/profile/components/UserInfo/index.tsx
@@ -1,5 +1,18 @@
-import { UserInfoContainer } from "./style";
-
+import Image from "next/image";
+import { ProfileDisplay, UserInfoContainer } from "./style";
+const AvatarExample =
+  "https://i0.wp.com/superdragonball.com.br/wp-content/uploads/2020/12/Por-que-Goku-nao-chamou-Paikuhan-para-o-Torneio-de-Poder.jpg?fit=1280%2C720&ssl=1";
 export function UserInfo() {
-  return <UserInfoContainer>Opa</UserInfoContainer>;
+  return (
+    <UserInfoContainer>
+      <ProfileDisplay>
+        <img src={AvatarExample} width={72} height={72} alt="" />
+
+        <div>
+          <section>Paikuhan Solador</section>
+          <p>membro desde 2023</p>
+        </div>
+      </ProfileDisplay>
+    </UserInfoContainer>
+  );
 }
--- bookwise/src/pages/user/profile/components/UserInfo/style.ts
+++ bookwise/src/pages/user/profile/components/UserInfo/style.ts
@@ -1,10 +1,43 @@
 import { styled } from "../../../../../../stiches.config";
 
-export const UserInfoContainer = styled('div', {
-    display: 'flex',
-    width: '100%',
-    height: '100%',
-    flexDirection: 'column',
-    marginLeft: '4rem',
-    backgroundColor: 'red'
-})
+export const UserInfoContainer = styled("div", {
+  display: "flex",
+  width: "100%",
+  height: "100%",
+  flexDirection: "column",
+  marginLeft: "4rem",
+
+  paddingTop: "6.8rem",
+});
+
+export const ProfileDisplay = styled("div", {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  flexDirection: "column",
+  gap: 24,
+  section: {
+    fontFamily: "$default",
+    fontSize: "$lg",
+    fontWeight: "$bold",
+    color: "$gray100",
+  },
+  img: {
+    borderRadius: 999,
+    border: " 2px solid rgba(127, 209, 204, 1)",
+  },
+
+  div: {
+    display: "flex",
+    flexDirection: "column",
+    textAlign: "center",
+    gap: 6,
+
+    p: {
+      color: "$gray400",
+      fontFamily: "$default",
+      fontSize: "$xs",
+      fontWeight: "$bold",
+    },
+  },
+});
