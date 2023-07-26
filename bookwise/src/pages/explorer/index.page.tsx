6b4f1689fafa015f1309bfa3331baef553376567
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-07-26 11:26:09 -0300

    movendo tudo pra uma página só para facilitar evitar muito propdrilling

--- bookwise/src/pages/explorer/index.page.tsx
+++ bookwise/src/pages/explorer/index.page.tsx
@@ -1,14 +1,266 @@
-import { ExplorerContainer } from "./style";
-import { BookCard } from "./components/BookCard";
+import { BookListCardContainer, BookListCardContent, BookListContainer, ExplorerContainer, GeneralTabButton, HeaderContainer, TagContainer } from "./style";
 import { NextSeo } from "next-seo";
+import { Binoculars, MagnifyingGlass, Star } from "phosphor-react";
+import { api } from "@/lib/axios";
+import { useState, useEffect } from "react";
+
+
+
+interface BookTags {
+  id: string;
+  name: string;
+}
+
+interface ExplorerProps {
+  id: string;
+
+  name: string;
+  author: string;
+  summary: string;
+  cover_url: string;
+
+  categories: [
+    categorie: {
+      category: {
+        name: string;
+      };
+    }
+  ];
+}
 
 export default function Explorer() {
+  const [bookTags, setBookTags] = useState<BookTags[]>([]);
+  const [explorerBooks, setExplorerBooks] = useState<ExplorerProps[]>([]);
+
+  async function fetchData() {
+    const responseTag = await api.get("books/category");
+    const response = await api.get("books/explorer");
+    setExplorerBooks(response.data);
+    setBookTags(responseTag.data);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function Teste(name: string) {
+    const FilterName = name
+    console.log(FilterName)
+ }
+
+ 
+
+ var searchString = "Todos"; // The string you want to filter by
+
+ const filteredList = explorerBooks.filter((obj) =>
+   obj.categories
+     .map((category) => category.category.name)
+     .includes(searchString)
+ );
+
+ console.log(filteredList);
+
   return (
     <>
       <NextSeo title="Explorar | BookWise" description="Página de Exploração" />
       <ExplorerContainer>
-        <BookCard />
+      <HeaderContainer>
+        <header>
+          <div>
+            <Binoculars />
+            Explorar
+          </div>
+          <section>
+            <input placeholder="Procure um livro" />
+            <MagnifyingGlass />
+          </section>
+        </header>
+      </HeaderContainer>
+      <TagContainer>
+      {bookTags.map((tag) => (
+        <>
+          <GeneralTabButton onClick={() => Teste(tag.name)}>
+            {tag.name}
+          </GeneralTabButton>
+        </>
+      ))}
+    </TagContainer>
+      <BookListContainer>
+     
+    <>
+      {searchString == "Todos"
+        ? explorerBooks.map((item) => (
+            <BookListCardContainer key={item.id}>
+              <img src={item.cover_url} alt="" width={108} height={152} />
+
+              <BookListCardContent>
+                <section>
+                  {item.name}
+                  <div>{item.author}</div>
+                  {item.categories.map((items) => (
+                    <div>{items.category.name.split(",")}</div>
+                  ))}
+                </section>
+
+                <p>
+                  <Star size={20} weight="fill" />
+                  <Star size={20} weight="fill" />
+                  <Star size={20} weight="fill" />
+                  <Star size={20} weight="fill" />
+                  <Star size={20} />
+                </p>
+              </BookListCardContent>
+            </BookListCardContainer>
+          ))
+        : filteredList.map((item) => (
+            <>
+              <BookListCardContainer key={item.id}>
+                <img src={item.cover_url} alt="" width={108} height={152} />
+
+                <BookListCardContent>
+                  <section>
+                    {item.name}
+                    <div>{item.author}</div>
+                    {item.categories.map((items) => (
+                      <div>{items.category.name.split(",")}</div>
+                    ))}
+                  </section>
+
+                  <p>
+                    <Star size={20} weight="fill" />
+                    <Star size={20} weight="fill" />
+                    <Star size={20} weight="fill" />
+                    <Star size={20} weight="fill" />
+                    <Star size={20} />
+                  </p>
+                </BookListCardContent>
+              </BookListCardContainer>
+            </>
+          ))}
+    </>
+      </BookListContainer>
+    
       </ExplorerContainer>
     </>
   );
 }
+
+
+
+
+/*
+  <>
+      {explorerBooks.map((item) => (
+        <BookListCardContainer key={item.id}>
+          <Image src={item.cover_url} alt="" width={108} height={152} />
+
+          <BookListCardContent>
+            <section>
+              {item.name}
+              <div>{item.author}</div>
+             {item.categories.map((items) => (
+              <div>{(items.category.name).split(',')}</div>
+             ))}
+            </section>
+
+            <p>
+              <Star size={20} weight="fill" />
+              <Star size={20} weight="fill" />
+              <Star size={20} weight="fill" />
+              <Star size={20} weight="fill" />
+              <Star size={20} />
+            </p>
+          </BookListCardContent>
+        </BookListCardContainer>
+      ))}
+    </>
+* */
+
+/*
+
+      {filteredList.map((item) => (
+        <>
+        
+         <BookListCardContainer key={item.id}>
+          <Image src={item.cover_url} alt="" width={108} height={152} />
+
+          <BookListCardContent>
+            <section>
+              {item.name}
+              <div>{item.author}</div>
+             {item.categories.map((items) => (
+              <div>{(items.category.name).split(',')}</div>
+             ))}
+            </section>
+
+            <p>
+              <Star size={20} weight="fill" />
+              <Star size={20} weight="fill" />
+              <Star size={20} weight="fill" />
+              <Star size={20} weight="fill" />
+              <Star size={20} />
+            </p>
+          </BookListCardContent>
+        </BookListCardContainer>
+        </>
+      ))}
+
+
+
+
+
+
+
+       <>
+        {searchString === "Todos" &&  
+         explorerBooks.map((item) => (
+          <BookListCardContainer key={item.id}>
+            <Image src={item.cover_url} alt="" width={108} height={152} />
+  
+            <BookListCardContent>
+              <section>
+                {item.name}
+                <div>{item.author}</div>
+               {item.categories.map((items) => (
+                <div>{(items.category.name).split(',')}</div>
+               ))}
+              </section>
+  
+              <p>
+                <Star size={20} weight="fill" />
+                <Star size={20} weight="fill" />
+                <Star size={20} weight="fill" />
+                <Star size={20} weight="fill" />
+                <Star size={20} />
+              </p>
+            </BookListCardContent>
+          </BookListCardContainer>
+        ))}   
+
+{filteredList.map((item) => (
+        <>
+        
+         <BookListCardContainer key={item.id}>
+          <Image src={item.cover_url} alt="" width={108} height={152} />
+
+          <BookListCardContent>
+            <section>
+              {item.name}
+              <div>{item.author}</div>
+             {item.categories.map((items) => (
+              <div>{(items.category.name).split(',')}</div>
+             ))}
+            </section>
+
+            <p>
+              <Star size={20} weight="fill" />
+              <Star size={20} weight="fill" />
+              <Star size={20} weight="fill" />
+              <Star size={20} weight="fill" />
+              <Star size={20} />
+            </p>
+          </BookListCardContent>
+        </BookListCardContainer>
+        </>
+      ))}
+    </>
+*/
--- bookwise/src/pages/explorer/style.ts
+++ bookwise/src/pages/explorer/style.ts
@@ -1,8 +1,170 @@
 import { styled } from "../../../stiches.config";
 
 export const ExplorerContainer = styled("section", {
-  display: "grid",
-  gridTemplateColumns: "auto 1fr",
-  gap: 64
- 
+  display: "flex",
+  flexDirection: "column", 
+
+  marginLeft: 64
+});
+
+
+
+export const BookCardContainer = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+  marginLeft: '4rem'
+})
+
+
+export const HeaderContainer = styled("header", {
+  header: {
+    marginTop: "0.75rem",
+    paddingLeft: 12,
+    display: "flex",
+    justifyContent: "space-between",
+    width: "62.25rem",
+    height: "2.125rem",
+
+    div: {
+      fontFamily: "$default",
+      fontSize: "$2xl",
+
+      display: "flex",
+
+      alignItems: "center",
+      gap: "0.75rem",
+
+      svg: {
+        color: "$green100",
+      },
+    },
+
+    section: {
+      width: 433,
+      height: 48,
+      borderRadius: 8,
+      backgroundColor: "transparent",
+      border: "1px solid $gray500",
+      display: "flex",
+      justifyContent: "space-between",
+      alignItems: "center",
+      ":hover": {
+        cursor: "pointer",
+      },
+      svg: {
+        width: "3rem",
+        color: "$gray500",
+        transition: "0.4s",
+      },
+
+      input: {
+        width: "100%",
+        height: "100%",
+        backgroundColor: "transparent",
+        border: "none",
+        marginLeft: "0.5rem",
+        color: "$gray100",
+        fontWeight: "bold",
+        fontFamily: "$default",
+      },
+
+      ":focus": {
+        outline: "none",
+      },
+    },
+  },
+});
+
+
+export const BookListContainer = styled('div', {
+  display : 'grid',
+  gridTemplateColumns: '1fr 1fr 1fr',
+  gap: '1rem'
+})
+
+
+
+export const BookListCardContainer = styled("div", {
+  width: '20rem',
+  height: '11.5rem',
+
+  backgroundColor: '$gray700',
+  borderRadius: 8,
+  padding: '1rem',
+  display: 'flex',
+
+  section: {
+   
+    fontFamily: '$default',
+    color: '$gray100',
+    fontWeight: "$bold",
+    
+    fontSize: '$md',
+  div: {
+    marginTop: "0.2rem",
+    fontFamily: '$default',
+    color: '$gray400',
+    fontSize: '$xs',
+}
+   
+  },
+
+})
+
+export const BookListCardContent = styled('div', {
+  display:'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  width: '100%',
+  marginLeft: '1.5rem',
+
+  p: {
+    svg: {
+      color: '$purple100'
+    }
+  }
+})
+
+
+
+export const TagContainer = styled("p", {
+  width: "100%",
+
+  height: "2.125rem",
+  display: "flex",
+  flexWrap: "wrap",
+  flexDirection: "row",
+  alignContent: "center",
+  marginTop: "4.5rem",
+  gap: "0.75rem",
+  marginBottom: "2.5rem",
+  ":hover": {
+    transition: "0.2s",
+    cursor: "pointer",
+    color: "$gray100",
+    border: "1px solid $gray100",
+  },
+});
+
+export const GeneralTabButton = styled("button", {
+    
+  backgroundColor: "transparent",
+  width: '100%',
+  maxWidth:'7rem',
+  height: '2rem',
+  border: "1px solid $purple100",
+  color: "$purple100",
+  borderRadius: "1rem",
+  variants: {
+    active: {
+      ButtonActive: {
+        border: "1px solid $purple200",
+        backgroundColor: "$purple200",
+        color: "$gray100",
+      },
+    },
+  },
+  ":focus": {
+    outline: "none",
+  },
 });
