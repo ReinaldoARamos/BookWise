d34fc6272336a5e08e0ae5109c7b1ee0c152661c
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-08-03 15:26:36 -0300

    criei um array.form que gera um array renderizando os icones de estrela, e agora quando ele clica ele pega o index, salva num estado e dela

--- bookwise/src/components/RatingStars/index.tsx
+++ bookwise/src/components/RatingStars/index.tsx
@@ -1,21 +1,32 @@
 import { Star } from "phosphor-react";
 import { StarContainer } from "./style";
 import { FaRegStar } from "react-icons/fa";
+import { useState } from "react";
 interface RatingStarsProps {
   size: number;
 }
 
-export const IconArray = ({ size } : RatingStarsProps) => {
-    const iconArray = Array.from({ length: size }, (_, index) => index); // Generate an array of specified size
-    function HandleIndexPosition() {
+export const IconArray = ({ size }: RatingStarsProps) => {
+ 
+    const [hoverIndex, setHoverIndex] = useState<number>(-1);
+  const iconArray = Array.from({ length: size }, (_, index) => index); 
 
-    }
-    return (
-  <StarContainer>
-        {iconArray.map((_, index) => (
-          <Star key={index} size={24} onClick={() => {console.log(index + 1)}}/> // Replace "IconName" with the specific icon component you imported.
-        ))}
-     </StarContainer>
-    );
+  const handleMouseOver = (index: number) => {
+    setHoverIndex(index + 1);
   };
-
+    return (
+      <StarContainer>
+          {iconArray.map((_, index) => (
+        <Star
+          key={index}
+          size={24}
+          onMouseEnter={() => handleMouseOver(index)}
+          onMouseLeave={() => handleMouseOver(-1)}
+          weight={(index + 1) <= hoverIndex ? "fill" : "regular"}
+          
+        />
+      ))}
+      </StarContainer>
+    );
+  
+};
