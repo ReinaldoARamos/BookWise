fed72f9590b7f7ebb243457c1a380d9d722def2f
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-08-11 08:16:40 -0300

    ajustando mais uma vez

--- bookwise/src/components/DrawerDialog/index.tsx
+++ bookwise/src/components/DrawerDialog/index.tsx
@@ -139,7 +139,7 @@ export function DrawerDialog({ children, bookId }: DrawerDialogProps) {
 
     setTimeout(() => {
       setLoading(true);
-    }, 500);
+    }, 700);
   }
 
   async function handleCreateReview() {
